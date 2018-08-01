461f6b86b2efa372dbffb689c1f3c41512491e68
HwangJaeSuk <dajasin245@naver.com>
2018-08-01 13:15:48 +0900

    login hwangjaesuk

--- happymoon/urls.py
+++ happymoon/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from store import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('store/pad/', views.product_list),
     path('store/pad/<int:pk>/', views.product_detail),
-
+    path('login/', include('login.urls')),
 ]
