144cfab3bb2c456f7d82d5781fa833933ec695d8
eunzz <eunzz123@gmail.com>
2018-08-03 15:42:00 +0900

    Create Branch store_pad

--- store/models.py
+++ store/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-  img = models.ImageField()
+  img_front = models.ImageField(upload_to = 'store/', blank=True)
+  img_back = models.ImageField(upload_to = 'store/', blank=True)
+  img_inner = models.ImageField(upload_to = 'store/', blank=True)
+  img_side = models.ImageField(upload_to = 'store/', blank=True)
   name = models.CharField(max_length=20)
   price = models.IntegerField()
   detail = models.TextField()
