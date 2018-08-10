8a8d4789191b26efeaa989170ebb15d60f768b42
eunzz <eunzz123@gmail.com>
2018-08-10 17:28:02 +0900

    0810

--- store/views.py
+++ store/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import Product
+from django.shortcuts import render,reverse, redirect
+from .models import Product, Cart_for_Pad
 from django.db.models import Q
 import random
 
@@ -13,17 +13,26 @@ def product_list(request):
 
 
 def product_detail(request, pk):
-  product = Product.objects.get(pk=pk)
-  
-  random_num=[]
-  for i in range(1,len(Product.objects.all())+1):
-    random_num.append(i)
-  del random_num[pk-1]
-  random.shuffle(random_num)
+    product = Product.objects.get(pk=pk)
+    if request.method == 'POST':
+        #user
+        order = request.POST.get('order')
+        cart = Cart_for_Pad.objects.create(
+            #user,
+            product = product,
+            order = order,
+        )
+        return  redirect(reverse('pad_list'))
+    else:
+        random_num=[]
+        for i in range(1,len(Product.objects.all())+1):
+            random_num.append(i)
+        del random_num[pk-1]
+        random.shuffle(random_num)
 
-  products = Product.objects.filter(Q(pk=random_num[0])|Q(pk=random_num[1])|Q(pk=random_num[2]))
-  ctx={
-    'product' : product,
-    'products' : products,
-  }
-  return render(request,'store/product_detail.html', ctx)
+        products = Product.objects.filter(Q(pk=random_num[0])|Q(pk=random_num[1])|Q(pk=random_num[2]))
+        ctx={
+            'product' : product,
+            'products' : products,
+        }
+        return render(request,'store/product_detail.html', ctx)
