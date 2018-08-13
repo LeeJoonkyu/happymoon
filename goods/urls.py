7abb5b86a44da668aab74174f254c4b9c7c1d718
eunzz <eunzz123@gmail.com>
2018-08-13 15:05:08 +0900

    First Merge

--- goods/urls.py
+++ goods/urls.py
@@ -2,6 +2,9 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
+
+app_name = 'store_goods'
+
 urlpatterns=[
 
     path('',views.goods_list),
--- store/urls.py
+++ store/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'store_pad'
+
 urlpatterns = [
     path('', views.product_list, name='pad_list'),
     path('<int:pk>/', views.product_detail, name='pad_detail'),
