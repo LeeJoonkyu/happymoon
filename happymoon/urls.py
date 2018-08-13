5b672dace0c462be5b9f9da106caf51c69312e22
eunzz <eunzz123@gmail.com>
2018-08-13 14:59:17 +0900

    temp for Push

--- happymoon/urls.py
+++ happymoon/urls.py
@@ -1,50 +1,17 @@
-<<<<<<< HEAD
-"""happy URL Configuration
-=======
-"""happymoon URL Configuration
->>>>>>> 7f5d1551887abe6fb3e1badd8233aae6a2a164d5
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import include, path
-<<<<<<< HEAD
-from django.conf import settings
-
-=======
 from store import views
 from django.conf import settings
 from django.conf.urls.static import static
->>>>>>> 7f5d1551887abe6fb3e1badd8233aae6a2a164d5
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('login/', include('login.urls')),
-]
-=======
-<<<<<<< HEAD
-    path('store/pad/', include('store.urls')),
-    path('store/goods/', include('goods.urls')),
+    path('login/', include('login.urls', namespace="login")),
+    path('store/pad/', include('store.urls', namespace="store_pad")),
+    path('store/goods/', include('goods.urls', namespace="store_goods")),
+    path('', include('subscription.urls', namespace="subscription")),
 
-=======
-    path('store/pad/', views.product_list),
-    path('store/pad/<int:pk>/', views.product_detail),
-    path('', include('subscription.urls')),
->>>>>>> subscription
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
->>>>>>> 7f5d1551887abe6fb3e1badd8233aae6a2a164d5
