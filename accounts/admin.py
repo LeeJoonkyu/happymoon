f8037bf455ae4c5c81434dc61cd6c16f1470f069
jaesuk <dajasin245@naver.com>
2018-08-16 23:27:33 +0900

    login&logout

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Information
 
-# Register your models here.
+@admin.register(Information)
+class InformationAdmin(admin.ModelAdmin):
+    list_display = ['name','email']
--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('signup/', views.information_create_view, name="signup"),
     path('join_success/', views.join_success, name="join_success"),
     path('login/', views.signin, name="login"),
-    path('logout/', auth_views.logout, name='logout', kwargs={'next_page': '/'}),
+    path('logout/', auth_views.logout, name='logout', kwargs={'next_page': '/accounts/login'}),
     ]
 
